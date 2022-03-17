09a3c5921df36c9cf2827d8a924b30a7536ce885
Matheus <matheusst82000@gmail.com>
2022-03-16 22:31:54 -0300

    Final Commit(for now)

--- Program.cs
+++ Program.cs
@@ -6,11 +6,74 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            
+            Menu();
+
             // Sum();
 
             // Subtraction();
             
            // Division();
+
+           // Multiplication();
+
+        }
+
+        static void Menu(){
+            
+            var check = 1;
+
+            while(Convert.ToBoolean(check) == true){
+                
+                comeback:
+
+                Console.Clear();
+
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("1 - Sum");
+                Console.WriteLine("2 - Subtraction");
+                Console.WriteLine("3 - Division");
+                Console.WriteLine("4 - Multiplication");
+
+                Console.WriteLine("-------------------------");
+                Console.WriteLine("Select an option: "); 
+
+                var res = short.Parse(Console.ReadLine());
+
+                  switch(res){
+                    case 1: Sum(); break;
+                    case 2: Subtraction(); break;
+                    case 3: Division(); break;
+                    case 4: Multiplication(); break;
+
+                    default: Console.WriteLine("Not found");
+                    break;
+                    }
+                    
+                if (!(res == 1) && !(res == 2) && !(res == 3) && !(res == 4))
+                {
+                    goto comeback;
+                }
+
+                comeback2:
+
+                Console.WriteLine("Would you like to do it again?(S/N)");
+
+                var res2 = Console.ReadLine();
+
+                if(res2 == "S"){
+                    
+                    goto comeback;
+                }
+                else if(!(res2 == "S") && !(res2 == "N")){
+
+                    Console.WriteLine("Not found");
+                    goto comeback2;
+                }else{
+                    check = 0;
+                }
+            }
+            Console.WriteLine("End!");
 
         }
 
@@ -65,6 +128,24 @@ namespace Calculator
             Console.WriteLine("");
 
             Console.WriteLine($"{num1} devided by {num2} is equal to: {num1 / num2}");
+
+            Console.ReadKey();
+        }
+
+        static void Multiplication(){
+            Console.Clear();
+
+            Console.WriteLine("Type the first value: ");
+
+            var num1 = Console.ReadLine();
+
+            Console.WriteLine("Type the second value: ");
+
+            var num2 = Console.ReadLine();
+
+            var result = double.Parse(num1) * double.Parse(num2);
+
+            Console.WriteLine($"The result of the multiplication is {result}");
 
             Console.ReadKey();
         }
